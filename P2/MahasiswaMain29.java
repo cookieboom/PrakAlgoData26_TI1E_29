20124b543730016fc0fd074d4e99e4de4d1ba0aa
cookieboom <satriarizky52@gmail.com>
2026-02-20 14:03:21 +0700

    P2 Percobaan 2

---
+++ P2/MahasiswaMain29.java
@@ -0,0 +1,16 @@
+package P2;
+
+public class MahasiswaMain29 {
+    public static void main(String[] args) {
+        Mahasiswa29 mhs1 = new Mahasiswa29();
+        mhs1.nama29 = "Muhammad Ali Farhan";
+        mhs1.nim29 = "2241720171";
+        mhs1.kelas29 = "SI 2J";
+        mhs1.ipk29 = 3.55;
+
+        mhs1.tampilakanInformasi29();
+        mhs1.ubahKelas29("SI 2K");
+        mhs1.updateIPK29(3.60);
+        mhs1.tampilakanInformasi29();
+    }
+}
