c651baeb4be11119f2370def16dbdc2baac162e3
cookieboom <satriarizky52@gmail.com>
2026-05-07 14:24:45 +0700

    Praktikum 11 percobaan 2

--- P11/SLLMain29.java
+++ P11/SLLMain29.java
@@ -17,30 +17,20 @@ public class SLLMain29 {
         sll.addLast(mhs1);
         sll.print();
         sll.insertAfter("Dirga", mhs3);
+        sll.print();
         sll.insertAt(2, mhs2);
         sll.print();
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Masukkan jumlah mahasiswa: ");
-        int n = sc.nextInt();
-        sc.nextLine();
+        System.out.println("data index 1: ");
+        sll.getData(1);
 
-        for (int i = 0; i < n; i++) {
-            System.out.println("Masukkan data mahasiswa ke-" + (i + 1) + ":");
-            System.out.print("NIM: ");
-            String nim = sc.nextLine();
-            System.out.print("Nama: ");
-            String nama = sc.nextLine();
-            System.out.print("Kelas: ");
-            String kelas = sc.nextLine();
-            System.out.print("IPK: ");
-            double ipk = sc.nextDouble();
-            sc.nextLine();
+        System.out.println("data mahasiswa an Bimon berada pada index: " + sll.indexOf("bimon"));
+        System.out.println();
 
-            Mahasiswa29 mhs = new Mahasiswa29(nim, nama, kelas, ipk);
-            sll.addLast(mhs);
-        }
+        sll.removeFirst();
+        sll.removeLast();
         sll.print();
-        sc.close();
+        sll.removeAt(0);
+        sll.print();
     }
 }
